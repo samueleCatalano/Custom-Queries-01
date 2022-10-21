7db727305a9cf7464f6c8c46f10aaef9829631a9
Samuele Catalano <csamuele73@mail.com>
2022-10-21 12:35:35 +0200

    first commit

---
+++ src/main/java/com/customqueries01/customqueries01/Customqueries01Application.java
@@ -0,0 +1,13 @@
+package com.customqueries01.customqueries01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Customqueries01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Customqueries01Application.class, args);
+	}
+
+}
